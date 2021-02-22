e5f5f5d94045909d7943319a5cec47ef6926ddca
Jeff Ortel <jortel@redhat.com>
2021-02-22 14:22:13 -0600

    Add watch support in web layer.

--- pkg/inventory/web/handler.go
+++ pkg/inventory/web/handler.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	liberr "github.com/konveyor/controller/pkg/error"
 	"github.com/konveyor/controller/pkg/inventory/container"
 	"github.com/konveyor/controller/pkg/inventory/model"
 	"net/http"
@@ -118,6 +120,133 @@ func (h *Authorized) setToken(ctx *gin.Context) {
 	if len(fields) == 2 && fields[0] == "Bearer" {
 		h.Token = fields[1]
 	}
+}
+
+//
+// Watched resource builder.
+type ResourceBuilder func(model.Model) interface{}
+
+//
+// Event
+type Event struct {
+	// Action.
+	Action int8
+	// Affected Resource.
+	Resource interface{}
+	// Updated resource.
+	Updated interface{}
+}
+
+//
+// Watch (event) writer.
+type WatchWriter struct {
+	// negotiated web socket.
+	webSocket *websocket.Conn
+	// model watch.
+	watch *model.Watch
+	// Resource.
+	builder ResourceBuilder
+}
+
+//
+// End.
+func (r *WatchWriter) end() {
+	_ = r.webSocket.Close()
+	r.watch.End()
+}
+
+//
+// Write event to the socket.
+func (r *WatchWriter) send(e model.Event) {
+	event := Event{
+		Action: e.Action,
+	}
+	if e.Model != nil {
+		event.Resource = r.builder(e.Model)
+	}
+	if e.Updated != nil {
+		event.Updated = r.builder(e.Updated)
+	}
+	err := r.webSocket.WriteJSON(event)
+	if err != nil {
+		r.end()
+	}
+}
+
+//
+// Watch has started.
+func (r *WatchWriter) Started() {
+}
+
+//
+// Watch has parity.
+func (r *WatchWriter) Parity() {
+	r.send(model.Event{})
+}
+
+//
+// A model has been created.
+func (r *WatchWriter) Created(event model.Event) {
+	r.send(event)
+}
+
+//
+// A model has been updated.
+func (r *WatchWriter) Updated(event model.Event) {
+	r.send(event)
+}
+
+//
+// A model has been deleted.
+func (r *WatchWriter) Deleted(event model.Event) {
+	r.send(event)
+}
+
+//
+// An error has occurred delivering an event.
+func (r *WatchWriter) Error(err error) {
+}
+
+//
+// An event watch has ended.
+func (r *WatchWriter) End() {
+}
+
+//
+// Watched (handler).
+type Watched struct {
+	WatchRequest bool
+}
+
+//
+// Prepare the handler to fulfil the request.
+// Set the `HasWatch` field using passed headers.
+func (h *Watched) Prepare(ctx *gin.Context) int {
+	_, h.WatchRequest = ctx.Request.Header[WatchHeader]
+	return http.StatusOK
+}
+
+//
+// Watch model.
+func (r *Watched) Watch(
+	ctx *gin.Context,
+	db model.DB,
+	m model.Model,
+	rb ResourceBuilder) (err error) {
+	//
+	upGrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	writer := &WatchWriter{builder: rb}
+	socket, err := upGrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	if err != nil {
+		err = liberr.Wrap(err)
+		return
+	}
+	writer.webSocket = socket
+	writer.watch, err = db.Watch(m, writer)
+	return
 }
 
 //
