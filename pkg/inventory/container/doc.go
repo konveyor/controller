eb46319c90656fbe56a35c8bdb3228c130d9368a
Jeff Ortel <jortel@redhat.com>
2021-07-13 17:24:27 -0500

    Reconciler renamed: Collector.

--- pkg/inventory/container/doc.go
+++ pkg/inventory/container/doc.go
@@ -1,10 +1,10 @@
 // Container
-//   |__Reconciler
-//   |__Reconciler
-//   |__Reconciler
+//   |__Collector
+//   |__Collector
+//   |__Collector
 //
-// The container is a collection of data model reconcilers.
-// Each reconciler is responsible for ensuring that changes made
+// The container is a collection of data model collectors.
+// Each collector is responsible for ensuring that changes made
 // to the external data source are reflected in the DB.  The
 // goal is for the data model to be eventually consistent.
 package container
@@ -13,6 +13,6 @@ package container
 // Build a new container.
 func New() *Container {
 	return &Container{
-		content: map[Key]Reconciler{},
+		content: map[Key]Collector{},
 	}
 }
